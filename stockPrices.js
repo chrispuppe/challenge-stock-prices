3ff94f178c7b4541054407d19172bcf702495199
Chris Puppe <chrispuppe@gmail.com>
2021-12-18 12:18:44 -0800

    challange complete and tested

--- stockPrices.js
+++ stockPrices.js
@@ -1,4 +1,9 @@
 const stockPrices = [15, 10, 20, 22, 1, 9]
+const stockPrices2 = [1, 2, 3, 4, 5]
+const stockPrices3 = [2, 3, 10, 6, 4, 8, 1]
+const stockPrices4 = [7, 9, 5, 6, 3, 2]
+const stockPrices5 = [0, 100]
+const stockPrices6 = [23, 4, 456, 23, 567, 7, 4532, 5, 45647, 4]
 
 function best(arr) {
     let maxProfit = 0
@@ -19,3 +24,8 @@ function best(arr) {
 }
 
 console.log(best(stockPrices))
+console.log(best(stockPrices2))
+console.log(best(stockPrices3))
+console.log(best(stockPrices4))
+console.log(best(stockPrices5))
+console.log(best(stockPrices6))
